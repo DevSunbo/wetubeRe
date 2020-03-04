7bd619f48003d04b937bae800ab3b6104df91763
DevSunbo <cowwhite1008@gmail.com>
2020-03-04 16:01:22 +0900

    express & babel & nodemon 설치

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hi");
+import express from "express";
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => {
+    console.log(`Listening on: ${PORT}`);
+}
+
+const handleHome= (req, res) =>{
+    console.log(req); 
+    res.send("hello")
+}
+
+app.get("/", handleHome);
+
+app.listen(4000, handleListening);
