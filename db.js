9d55b88843e40251d710a2907512430f4a84244d
DevSunbo <cowwhite1008@gmail.com>
2020-03-05 20:37:30 +0900

    mongoose 설치 & localhost:27017로 연결 & init에서 db 불러오기

--- db.js
+++ db.js
@@ -1,50 +1,14 @@
-export const videos = [
-    {
-        id:324393,
-        title: 'Video awesome',
-        description: 'This is something I love',
-        views:24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 122121,
-            name: "sim sunbo",
-            email: "co@naver.com"
-        }
-    },
-    {
-        id:1342124,
-        title: 'Video nice',
-        description: 'This is something I love',
-        views:24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 122121,
-            name: "sim sunbo",
-            email: "co@naver.com"
-        }
-    },
-    {
-        id:324393,
-        title: 'Video perfect',
-        description: 'This is something I love',
-        views:24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 122121,
-            name: "sim sunbo",
-            email: "co@naver.com"
-        }
-    },
-    {
-        id:324393,
-        title: 'Video super',
-        description: 'This is something I love',
-        views:24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 122121,
-            name: "sim sunbo",
-            email: "co@naver.com"
-        }
-    }
-];
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/we-tube", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("connect to db");
+const handleError = (error) => console.log(`error ${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
