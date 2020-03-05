1490af7a8ff626be0d998e362ef4936e473aa239
DevSunbo <cowwhite1008@gmail.com>
2020-03-05 17:45:06 +0900

    home,pug 에 video 배열 출력 & 가짜 db 만들어서 결과 확인

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id:324393,
+        title: 'Video awesome',
+        description: 'This is something I love',
+        views:24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 122121,
+            name: "sim sunbo",
+            email: "co@naver.com"
+        }
+    },
+    {
+        id:1342124,
+        title: 'Video nice',
+        description: 'This is something I love',
+        views:24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 122121,
+            name: "sim sunbo",
+            email: "co@naver.com"
+        }
+    },
+    {
+        id:324393,
+        title: 'Video perfect',
+        description: 'This is something I love',
+        views:24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 122121,
+            name: "sim sunbo",
+            email: "co@naver.com"
+        }
+    },
+    {
+        id:324393,
+        title: 'Video super',
+        description: 'This is something I love',
+        views:24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 122121,
+            name: "sim sunbo",
+            email: "co@naver.com"
+        }
+    }
+];
